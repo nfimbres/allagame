ddfb6ccc6e857735c176710562b7b4f861e15e56
nfimbres <nathanefimbres@gmail.com>
2025-08-15 18:08:03 +0000

    modified:   pages/player.php

--- pages/player.php
+++ pages/player.php
@@ -1,24 +1,27 @@
-<?php
+<body class="d-flex justify-content-center align-items-start" style="background: #f6f6f6;height: 800px;">
+    <div class="container d-flex justify-content-center" style="--bs-primary: rgb(8, 41, 114);--bs-primary-rgb: 8,41,114;--bs-secondary: rgb(234, 105, 12);--bs-secondary-rgb: 234,105,12;margin: 10px;padding: 0px;padding-top: 100px;padding-right: 20px;padding-bottom: 20px;padding-left: 20px;height: 800px;margin-right: 0px;margin-left: 0px;">
+        <?php
 
-$player = isset($_GET['player']) ? htmlspecialchars($_GET['player']) : null;
+        $player = isset($_GET['player']) ? htmlspecialchars($_GET['player']) : null;
 
-include __DIR__ . '/../../private_html/db-connection.php';
+        include __DIR__ . '/../../private_html/db-connection.php';
 
-if ($player !== null) {
-    // New SQL query to get player info
-    $sql = "SELECT fullName AS `Player`, playerId AS `Player ID` FROM players WHERE playerId = '" . $player . "'";
-    $result = $conn->query($sql);
-    if ($result && $row = $result->fetch_assoc()) {
-        $playerId = $row['Player ID'];
-        $playerName = $row['Player'];
-        $imgPath = "/../assets/img/players/{$playerId}.png";
-        $imgFullPath = __DIR__ . "/../assets/img/players/{$playerId}.png";
-        if (!file_exists($imgFullPath)) {
-            $imgPath = "/../assets/img/players/default.png";
+        if ($player !== null) {
+            // New SQL query to get player info
+            $sql = "SELECT fullName AS `Player`, playerId AS `Player ID` FROM players WHERE playerId = '" . $player . "'";
+            $result = $conn->query($sql);
+            if ($result && $row = $result->fetch_assoc()) {
+                $playerId = $row['Player ID'];
+                $imgPath = "/../assets/img/players/{$playerId}.png";
+                $imgFullPath = __DIR__ . "/../assets/img/players/{$playerId}.png";
+                if (!file_exists($imgFullPath)) {
+                    $imgPath = "/../assets/img/players/default.png";
+                }
+                echo "<img src='" . htmlspecialchars($imgPath) . "' alt='Player' style='height:10rem;width:10rem;object-fit:cover;border-radius:10rem;'>";
+                echo "<h1 style=\"padding-bottom: 10px;font-family: 'Roboto Condensed';color: var(--bs-primary);\">" . htmlspecialchars($row['Player']) . "</h1>";
+            }
         }
-    }
-} else {
-    header("Location: ?page=404");
-    exit();
-}
-?>
+
+        ?>
+    </div>
+</body>
